4b32119a8d053e1b5f314dc344e6c0c208c0e138
techraiders <hello_navneet@hotmail.com>
2016-12-21 00:23:22 +0530

    Toggles sidemenu.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
+function openSideMenu() {
     document.getElementById("sideMenu").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("openBtn").style.display = 'none';
+    document.getElementById("closeBtn").style.display = 'block';
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
+function closeSideMenu() {
     document.getElementById("sideMenu").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
+    document.getElementById("openBtn").style.display = 'block';
+    document.getElementById("closeBtn").style.display = 'none';
 }
