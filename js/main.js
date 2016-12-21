d26b8010bee85b49d866a55faa025d40f2f05f7c
techraiders <hello_navneet@hotmail.com>
2016-12-21 15:00:29 +0530

    Completes action buttons and filters.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openSideMenu() {
-    document.getElementById("sideMenu").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sideMenu").style.width = "210px";
+    document.getElementById("main").style.marginLeft = "210px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     document.getElementById("openBtn").style.display = 'none';
     document.getElementById("closeBtn").style.display = 'block';
